e6c2bb7dab36e316e71699627119046f7f12d273
AsesorAFT <125296722+AsesorAFT@users.noreply.github.com>
2025-09-05 17:44:44 -0600

    Publicar versión lista de la app

--- src/app/api/paypal/setup-billing/route.ts
+++ src/app/api/paypal/setup-billing/route.ts
@@ -1,6 +1,8 @@
 
 import { NextResponse } from 'next/server';
-import paypal from '@paypal/checkout-server-sdk';
+import paypal from '@paypal/paypal-server-sdk';
+// o
+import { Subscriptions } from '@paypal/paypal-server-sdk';
 
 /**
  * API endpoint to handle setting up a PayPal billing agreement/subscription.
